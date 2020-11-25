3cb3c5f509db59f2039782f683a5bc8528e2ecd7
Laila <lailaa@kth.se>
2020-11-25 12:07:58 +0100

    welcomePage draft

--- src/components/main.js
+++ src/components/main.js
@@ -1,16 +1,18 @@
 import React from "react";
 import Sidebar from "./sidebar/sidebar";
 import Navbar from "./navbar/navbar";
+import Welcome from "./welcome/welcome";
 
 const Main = () => (
 	<React.Fragment>
 		<div className="landingpage">
 			<div className="application">
-				<Navbar />
-				<Sidebar />
+			<Welcome/>
 			</div>
 		</div>
 	</React.Fragment>
 );
 
 export default Main;
+//<Navbar />
+//<Sidebar />
