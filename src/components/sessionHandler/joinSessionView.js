fc82508272f675fefa3acfc330c3e1c56feeee83
alphaf <>
2020-11-30 14:25:26 +0100

    Complete refactoring of welcome  and adding of sessionhandler together with its Views

---
+++ src/components/sessionHandler/joinSessionView.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Sidebar from "../sidebar/sidebar";
+import Navbar from "../navbar/navbar";
+import css from "./joinSessionView.module.css";
+import { Col, Container, Button, Jumbotron } from "react-bootstrap";
+
+const JoinSessionView = ({ user, logout }) => {
+	return (
+		<Container fluid className={css.sessionContainer}>
+			<Sidebar />
+			<Col>
+				<Navbar user={user} logout={logout} />
+				<Jumbotron className={css.sessionJumbo}>
+					<h1>Hello, @{user.displayName}!</h1>
+					<p>
+						Thank you for using Queueify, if you klick the button
+						below you can join your Hosts Session.
+					</p>
+					<p>
+						<Button variant="outline-success" href="#joinsession">
+							Join a Session
+						</Button>
+					</p>
+				</Jumbotron>
+			</Col>
+		</Container>
+	);
+};
+
+export default JoinSessionView;
