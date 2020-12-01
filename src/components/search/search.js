f1e42c2190f64259e901c0ecf9414d534c292e0c
mhansbo <mhansbo@kth.se>
2020-12-01 10:53:02 +0100

    update with static spotify

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -6,6 +6,35 @@ const { default: searchView } = require("./searchView");
 function Search({}){
     const [query, setQuery]= React.useState("");
     const [promise, setPromise]= React.useState(null);
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [items, setItems] = useState([]);
+  
+    function searchSong(e) {
+      e.preventDefault();
+      fetch("https://api.spotify.com/v1/search?q=" + query + "&type=track", {
+        method: "GET",
+        headers: {
+          Authorization: "Bearer " + "BQA8ZdlFG49N6iHu_m2kq5rEmv-HM103eCeumOncmFxqHCvkKOKzKYFDz0t2fVexWnp2JQQPKN9NHXE_0suaEd4VEkq0BY9sJt6JiTmLkv7qgbbzjJU_i2mSiCw1Vl0L1HYizADVbzQrjhk2UsIBcH3Gd3mxtgYru3PAjBErbiWCC6I8EluqYOJZblx2bEMpqBOGyA",
+        },
+      })
+        .then((response) => response.json())
+        .then(
+          (result) => {
+            setIsLoaded(true);
+            setItems(result);
+          },
+          (error) => {
+            setIsLoaded(true);
+            setError(error);
+          }
+        );
+      console.log(items);
+    }
+  
+    function addSong(track) {
+      return track;
+    }
 
 return(
    <SearchView
