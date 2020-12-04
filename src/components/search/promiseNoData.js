1174714c44748e15ea4d896c2ccaed1978736c23
alphaf <>
2020-12-04 22:22:45 +0100

    fix PromiseNoData/SearchViewResult and format code

--- src/components/search/promiseNoData.js
+++ src/components/search/promiseNoData.js
@@ -1,12 +1,19 @@
 import React from "react";
-import {Jumbotron, Spinner} from "react-bootstrap";
+import { Jumbotron, Spinner } from "react-bootstrap";
+import css from "./search.module.css";
 
-function PromiseNoData(promise, data, error, h){  
-    return  ((!promise && "no data" )    // case "0"  
-          || (error && <h1>error</h1>  ) // case 3 
-          ||( !data && <Jumbotron className="jumbo"><Spinner animation="border" role="status">
-          <span className="sr-only">Loading...</span>
-        </Spinner></Jumbotron>) )    // case 1
+function PromiseNoData(promise, data, error, h) {
+	return (
+		(!promise && "no data") || // case "0"
+		(error && <h1>error</h1>) || // case 3
+		(!data && (
+			<Jumbotron className={css.jumbo}>
+				<Spinner animation="border" role="status">
+					<span className="sr-only">Loading...</span>
+				</Spinner>
+			</Jumbotron>
+		))
+	); // case 1
 }
 
 export default PromiseNoData;
