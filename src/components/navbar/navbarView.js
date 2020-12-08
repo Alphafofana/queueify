4e5aff77bf18cd97fdd2a4c4e1b25600f9d8514d
mhansbo <mhansbo@kth.se>
2020-12-08 16:32:38 +0100

    Navbar and Sidebar same color

--- src/components/navbar/navbarView.js
+++ src/components/navbar/navbarView.js
@@ -4,7 +4,7 @@ import css from "./navbar.module.css";
 
 const Navibar = ({ user, logout }) => {
 	return (
-		<Navbar className={css.navbar} fixed="top" bg="dark" variant="dark">
+		<Navbar className={css.navbar} fixed="top" variant="dark">
 			<Container >
 				<Col lg={7}>
 					<Nav className="mr-auto"></Nav>
