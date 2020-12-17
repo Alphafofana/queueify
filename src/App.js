d8e8f4db1bc39a8fd77651fc5baaf7e4fc9e0f27
ellaso <ellaso@kth.se>
2020-12-17 19:22:50 +0100

    all unallowed routes after login will lead to /session

--- src/App.js
+++ src/App.js
@@ -14,106 +14,54 @@ import CurrentSession from "./components/currentSession/currentSession";
 import { Row, Col } from "react-bootstrap";
 
 import {
-	//HashRouter,
-	Redirect,
-	Switch,
-	Route,
-	BrowserRouter,
+  //HashRouter,
+  Redirect,
+  Switch,
+  Route,
+  BrowserRouter,
 } from "react-router-dom";
 
 function App() {
-	const model = ReadModel();
-	return (
-		<BrowserRouter>
-			<AuthProvider>
-				<Switch>
-					<Route exact path="/">
-						<Redirect to="/login" />
-					</Route>
-					<Route exact path="/login">
-						<Login />
-					</Route>
-					<Route exact path="/login/popup">
-						<Popup />
-					</Route>
-				<PrivateRoute>
-					<div className="sidebarCol">
-						<Sidebar className="sidebar" model={model} />
-						<div className="pageCol">
-							<Navbar />
-							<Switch>
-								<PrivateRoute exact path="/search">
-									<Search model={model} />
-								</PrivateRoute>
-								<PrivateRoute exact path="/session">
-									<SessionHandler model={model} />
-								</PrivateRoute>
-								<PrivateRoute exact path="/session/:sessionId">
-									<CurrentSession model={model} />
-								</PrivateRoute>
-							</Switch>
-						</div>
-					</div>
-				</PrivateRoute>
-				</Switch>
-			</AuthProvider>
-		</BrowserRouter>
-
-		/* 		<BrowserRouter>
-			<AuthProvider>
-				<Switch>
-					<Route exact path="/">
-						<Redirect to="/login" />
-					</Route>
-					<Route exact path="/login">
-						<Login />
-					</Route>
-					<Route exact path="/login/popup">
-						<Popup />
-					</Route>
-					<PrivateRoute exact path="/session">
-						<div className="pageContainer">
-							<Navbar model={model} />
-							<Row>
-								<Col lg="auto" className="sidebarCol">
-									<Sidebar model={model} />
-								</Col>
-								<Col className="pageCol">
-									<SessionHandler model={model} />
-								</Col>
-							</Row>
-						</div>
-					</PrivateRoute>
-					<PrivateRoute exact path="/session/:sessionId">
-						<div className="pageContainer">
-							<Navbar model={model} />
-							<Row>
-								<Col lg="auto" className="sidebarCol">
-									<Sidebar model={model} />
-								</Col>
-								<Col className="pageCol">
-									<CurrentSession model={model} />
-								</Col>
-							</Row>
-						</div>
-					</PrivateRoute>
-					<PrivateRoute exact path="/search">
-						<div className="pageContainer">
-							<Navbar />
-							<Row>
-								<Col lg="auto" className="sidebarCol">
-									<Sidebar model={model} />
-								</Col>
-								<Col className="pageCol">
-									<Search model={model} />
-								</Col>
-							</Row>
-						</div>
-					</PrivateRoute>
-				</Switch>
-			</AuthProvider>
-		</BrowserRouter> */
-	);
+  const model = ReadModel();
+  return (
+    <BrowserRouter>
+      <AuthProvider>
+        <Switch>
+          <Route exact path="/">
+            <Redirect to="/login" />
+          </Route>
+          <Route exact path="/login">
+            <Login />
+          </Route>
+          <Route exact path="/login/popup">
+            <Popup />
+          </Route>
+          <PrivateRoute>
+            <div className="sidebarCol">
+              <Sidebar className="sidebar" model={model} />
+              <div className="pageCol">
+                <Navbar />
+                <Switch>
+                  <PrivateRoute exact path="/search">
+                    <Search model={model} />
+                  </PrivateRoute>
+                  <PrivateRoute exact path="/session">
+                    <SessionHandler model={model} />
+                  </PrivateRoute>
+                  <PrivateRoute exact path="/session/:sessionId">
+                    <CurrentSession model={model} />
+                  </PrivateRoute>
+                  <PrivateRoute>
+                    <Redirect to="/session" />
+                  </PrivateRoute>
+                </Switch>
+              </div>
+            </div>
+          </PrivateRoute>
+        </Switch>
+      </AuthProvider>
+    </BrowserRouter>
+  );
 }
 
 export default App;
